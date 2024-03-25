7d5daa1e704712e3555546e1cc155a16e00c3dd3
CamiloCabezas <116015687+CamiloCabezas@users.noreply.github.com>
2024-03-24 20:02:18 -0500

    Agregar archivos de proyecto.

---
+++ Sofware Empresarial/Login.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Sofware_Empresarial
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.textDocumento = new System.Windows.Forms.TextBox();
+            this.textContraseña = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.IngresarButton = new FontAwesome.Sharp.IconButton();
+            this.CancelarButton = new FontAwesome.Sharp.IconButton();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.SteelBlue;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 232);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.SteelBlue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SISTEMA DE VENTAS";
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.SteelBlue;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Store;
+            this.iconPictureBox1.IconColor = System.Drawing.Color.White;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.IconSize = 130;
+            this.iconPictureBox1.Location = new System.Drawing.Point(51, 30);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(149, 130);
+            this.iconPictureBox1.TabIndex = 2;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // textDocumento
+            // 
+            this.textDocumento.Location = new System.Drawing.Point(279, 66);
+            this.textDocumento.Name = "textDocumento";
+            this.textDocumento.Size = new System.Drawing.Size(207, 20);
+            this.textDocumento.TabIndex = 3;
+            // 
+            // textContraseña
+            // 
+            this.textContraseña.Location = new System.Drawing.Point(279, 117);
+            this.textContraseña.Name = "textContraseña";
+            this.textContraseña.PasswordChar = '*';
+            this.textContraseña.Size = new System.Drawing.Size(207, 20);
+            this.textContraseña.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(276, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nro. Documento :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(276, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Contraseña :";
+            // 
+            // IngresarButton
+            // 
+            this.IngresarButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.IngresarButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.IngresarButton.ForeColor = System.Drawing.Color.White;
+            this.IngresarButton.IconChar = FontAwesome.Sharp.IconChar.DoorOpen;
+            this.IngresarButton.IconColor = System.Drawing.Color.White;
+            this.IngresarButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.IngresarButton.IconSize = 21;
+            this.IngresarButton.Location = new System.Drawing.Point(279, 163);
+            this.IngresarButton.Name = "IngresarButton";
+            this.IngresarButton.Size = new System.Drawing.Size(83, 35);
+            this.IngresarButton.TabIndex = 7;
+            this.IngresarButton.Text = "Ingresar";
+            this.IngresarButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.IngresarButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.IngresarButton.UseVisualStyleBackColor = false;
+            this.IngresarButton.Click += new System.EventHandler(this.IngresarButton_Click);
+            // 
+            // CancelarButton
+            // 
+            this.CancelarButton.BackColor = System.Drawing.Color.DarkRed;
+            this.CancelarButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CancelarButton.ForeColor = System.Drawing.Color.White;
+            this.CancelarButton.IconChar = FontAwesome.Sharp.IconChar.TimesCircle;
+            this.CancelarButton.IconColor = System.Drawing.Color.White;
+            this.CancelarButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.CancelarButton.IconSize = 21;
+            this.CancelarButton.Location = new System.Drawing.Point(403, 163);
+            this.CancelarButton.Name = "CancelarButton";
+            this.CancelarButton.Size = new System.Drawing.Size(83, 35);
+            this.CancelarButton.TabIndex = 8;
+            this.CancelarButton.Text = "Cancelar";
+            this.CancelarButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.CancelarButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.CancelarButton.UseVisualStyleBackColor = false;
+            this.CancelarButton.Click += new System.EventHandler(this.CancelarButton_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 232);
+            this.Controls.Add(this.CancelarButton);
+            this.Controls.Add(this.IngresarButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textContraseña);
+            this.Controls.Add(this.textDocumento);
+            this.Controls.Add(this.iconPictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private System.Windows.Forms.TextBox textDocumento;
+        private System.Windows.Forms.TextBox textContraseña;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private FontAwesome.Sharp.IconButton IngresarButton;
+        private FontAwesome.Sharp.IconButton CancelarButton;
+    }
+}
---
+++ Sofware Empresarial/Login.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sofware_Empresarial
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelarButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void IngresarButton_Click(object sender, EventArgs e)
+        {
+            Inicio form = new Inicio();
+
+            form.Show();
+            this.Hide();
+
+            form.FormClosing += forClosing;
+        }
+
+        private void forClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Show();
+        }
+    }
+}
